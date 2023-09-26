44a3dac86cd56ef8f7f722452c8b0faa2d011952
PTwr <PTwr@outlook.com>
2023-09-26 18:04:29 +0200

    TL Versus

---
+++ Patcher/Translation/Patch/DATA/files/_2d/Versus/Versus_lua.arc/arc/d_mapinfo.en.lua
@@ -0,0 +1,134 @@
+D_MapInfo = {
+	{
+name = "Near Odessa",
+code = "VSAB",
+code2 = "VTAB",
+openinfo = "Fighting near Odessa.",
+closeinfo = "This map cannot be selected yet.",
+	},
+	{
+name = "Ruined City",
+code = "VSAH",
+code2 = "VTAH",
+openinfo = "Fight in a ruined city.",
+closeinfo = "This map cannot be selected yet.",
+	},
+	{
+name = "desert area",
+code = "VSAF",
+code2 = "VTAF",
+openinfo = "Fight in the desert.",
+closeinfo = "This map cannot be selected yet.",
+	},
+	{
+name = "Mining Base",
+code = "VSAG",
+code2 = "VTAG",
+openinfo = "Fight at the mine base.",
+closeinfo = "This map cannot be selected yet.",
+	},
+	{
+name = "Valley Area 526",
+code = "VSAL",
+code2 = "VTAL",
+openinfo = "The battle will take place in Valley Area 526.",
+closeinfo = "This map cannot be selected yet.",
+	},
+	{
+name = "Snowfield area",
+code = "VSAJ",
+code2 = "VTAJ",
+openinfo = "Fight in the snowy field.",
+closeinfo = "This map cannot be selected yet.",
+	},
+	{
+name = "urban area",
+code = "VSAA",
+code2 = "VTAA",
+openinfo = "Fight in the city.",
+closeinfo = "This map cannot be selected yet.",
+	},
+	{
+name = "Belfast Base",
+code = "VSAK",
+code2 = "VTAK",
+openinfo = "Fight at Belfast base.",
+closeinfo = "This map cannot be selected yet.",
+	},
+	{
+name = "Arctic Base",
+code = "VSAE",
+code2 = "VTAE",
+openinfo = "Fight at the North Pole base.",
+closeinfo = "This map cannot be selected yet.",
+	},
+	{
+name = "Jaburo ground",
+code = "VSAC",
+code2 = "VTAC",
+openinfo = "Jaburo will fight on the ground.",
+closeinfo = "This map cannot be selected yet.",
+	},
+	{
+name = "Jaburo Underground",
+code = "VSAD",
+code2 = "VTAD",
+openinfo = "Fight in Jaburo underground.",
+closeinfo = "This map cannot be selected yet.",
+	},
+	{
+name = "California Bass",
+code = "VSAI",
+code2 = "VTAI",
+openinfo = "Fighting on California base.",
+closeinfo = "This map cannot be selected yet.",
+	},
+	{
+name = "random",
+code = "",
+code2 = "",
+openinfo = "Randomly select a map to battle on.",
+closeinfo = "This map cannot be selected yet.",
+	},
+}
+
+
+D_TiMeLimit = {
+	{
+		disp	= "1:00:00",
+		frame	= 1*60*60,
+	},
+	{
+		disp	= "2:00:00",
+		frame	= 2*60*60,
+	},
+	{
+		disp	= "3:00:00",
+		frame	= 3*60*60,
+	},
+	{
+		disp	= "4:00:00",
+		frame	= 4*60*60,
+	},
+	{
+		disp	= "5:00:00",
+		frame	= 5*60*60,
+	},
+	{
+		disp	= "NO LIMIT",
+		frame	= 0,
+	},
+}
+
+
+D_Round = {
+	{
+		count	= 1,
+	},
+	{
+		count	= 2,
+	},
+	{
+		count	= 3,
+	},
+}
