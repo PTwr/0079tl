d5f00745af9585948d36b5adfb78e4634e76cfcb
PTwr <PTwr@outlook.com>
2023-09-26 16:12:35 +0200

    TL Ace/Amuro pause objectives

---
+++ Patcher/Translation/Patch/DATA/files/_2d/Pause/PAUSE_A.arc/arc/AA01_text.en.lua
@@ -0,0 +1,11 @@
+MissionText = {
+	{
+		code = "AA01_EV01",
+		value = "Search and destroy enemy troops",
+	},
+	{
+		code = "AA01_EV02",
+		value = "A Red Comet has appeared! !\
+Defeat him! !",
+	},
+}
