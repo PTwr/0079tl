c4b9287547287fac7be559bb47860d7bc3d9dd94
PTwr <PTwr@outlook.com>
2023-09-26 18:03:29 +0200

    campaign hangar tl

---
+++ Patcher/Translation/Patch/DATA/files/_2d/Hangar/Hangar_lua.arc/arc/d_formationname.en.lua
@@ -0,0 +1,32 @@
+D_FormationName = {
+	{
+		name = "Wedge(Normal)", 
+	},
+	{
+		name = "Wedge(Offense)", 
+	},
+	{
+		name = "Wedge(Defense)", 
+	},
+	{
+		name = "Ｖ(Normal)", 
+	},
+	{
+		name = "Ｖ(Offense)", 
+	},
+	{
+		name = "Ｖ(Defense)", 
+	},
+	{
+		name = "Spread(Normal)", 
+	},
+	{
+		name = "Spread(Offense)", 
+	},
+	{
+		name = "Spread(Defense)", 
+	},
+	{
+		name = "--remove from list--", 
+	},
+}
