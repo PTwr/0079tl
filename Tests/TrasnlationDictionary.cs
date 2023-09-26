dca0588e6b68b60cc4c414f395996308156abf79
PTwr <PTwr@outlook.com>
2023-09-26 12:12:30 +0200

    translation dict

---
+++ Tests/TrasnlationDictionary.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class TrasnlationDictionary
+    {
+        [Fact]
+        public void PrepareTLJson()
+        {
+            var soundFiles = Directory.EnumerateFiles(@"C:\games\wii\0079\0079_en\DATA\files\sound\stream", "*.brstm");
+
+            var structure = soundFiles
+                .Select(s => new FileInfo(s))
+                .Select(fi => new
+                {
+                    FileName = "sound/stream/" + fi.Name,
+                    Translation = fi.Name,
+                    Miliseconds = 1000 * 5,
+                    Color = 4278255615, //cyan
+                    Enabled = !fi.Name.Contains("bgm"),
+                    AllowDuplicate = false,
+                })
+                .ToArray();
+
+            var json = JsonConvert.SerializeObject(structure, Formatting.Indented);
+            File.WriteAllText(@"C:\games\wii\translate.json", json);
+        }
+    }
+}
