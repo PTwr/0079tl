680aa4a090e3cbc7d10b7cb9568c038d7561a848
PTwr <PTwr@outlook.com>
2023-09-26 18:04:21 +0200

    TL Versus mode

---
+++ Patcher/Translation/Patch/DATA/files/_2d/Versus/Versus_lua.arc/arc/d_think.en.lua
@@ -0,0 +1,20 @@
+--difficulty level?
+D_ThinkName = {
+	{
+		code = "THI", 
+		name = "Easy", 
+	},
+	{
+		code = "THI", 
+		name = "Medium", 
+	},
+	{
+		code = "THI", 
+		name = "Hard", 
+	},
+	{
+		code = "THI", 
+		name = "Ace", 
+	},
+
+}
