7919f43b865af0d876acc0da249f6e2fe097b546
PTwr <PTwr@outlook.com>
2023-09-28 17:57:22 +0200

    ME01 dynamic objectives

---
+++ Patcher/Translation/Common/Lua/PauseScreenObjectivies/ME01_text.en.lua
@@ -0,0 +1,11 @@
+MissionText = {
+	{
+		code = "ME01_EV01",
+		value = "Search and destroy enemy forces.",
+	},
+	{
+		code = "ME01_EV02",
+		value = "Reinforcements from Gau Carrier!\
+Destroy them.",
+	},
+}
