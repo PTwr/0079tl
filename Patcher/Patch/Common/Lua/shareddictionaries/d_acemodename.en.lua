11c8b8ab7a39e1896188c31957bd608a313afda4
PTwr <PTwr@outlook.com>
2023-09-30 01:02:12 +0200

    ace acquisition fix

---
+++ Patcher/Patch/Common/Lua/shareddictionaries/d_acemodename.en.lua
@@ -0,0 +1,45 @@
+D_AceModeName = {
+	{
+		code = "AA01", 
+		name = "Amuro Ray", 
+	},
+	{
+		code = "AK01", 
+		name = "Christina Mackenzie", 
+	},
+	{
+		code = "AS01", 
+		name = "Shiro Amada", 
+	},
+	{
+		code = "AY01", 
+		name = "Yuu Kajima", 
+	},
+	
+	{
+		code = "AC01", 
+		name = "Char Aznable", 
+	},
+	
+	{
+		code = "AR01", 
+		name = "Ramba Ral", 
+	},
+	
+	{
+		code = "AG01", 
+		name = "Gaia", 
+	},
+	{
+		code = "AH01", 
+		name = "Akahana", 
+	},
+	{
+		code = "AN01", 
+		name = "Norris Packard", 
+	},
+	{
+		code = "AB01", 
+		name = "Bernard Wiseman", 
+	},
+}
