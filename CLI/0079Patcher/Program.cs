e26c6d83ebe237ee58851a0f35137c0b3ac22c64
PTwr <PTwr@outlook.com>
2023-09-29 21:08:18 +0200

    generate TL dicts for all missions

--- CLI/0079Patcher/Program.cs
+++ CLI/0079Patcher/Program.cs
@@ -7,6 +7,19 @@ using Newtonsoft.Json;
 
 internal class Program
 {
+    [Verb("prepare", HelpText = "Extracts human-readable form for translation.")]
+    public class ExtractionOptions
+    {
+        [Option('i', "input", Required = true, HelpText = "Directory containing unpacked game files")]
+        public string InputDir { get; set; }
+        [Option('o', "output", Required = true, HelpText = "Directory for patch files")]
+        public string OutputDir { get; set; }
+        [Option('o', "overwrite", Required = false, HelpText = "Overwrites existing files", Default = false)]
+        public bool Overwrite { get; set; }
+        [Option('l', "language", Required = false, HelpText = "Language code, defaults to en", Default = "en")]
+        public string LanguageCode { get; set; }
+    }
+    //[Verb("patch", HelpText = "Create game patch from translation")]
     public class Options
     {
         [Option('i', "input", Required = true, HelpText = "Directory containing clean game files")]
@@ -24,14 +37,18 @@ internal class Program
     }
     private static void Main(string[] args)
     {
-        //if (args?.Any() == false)
-        //{
-        //    var h = new HelpText().AddOptions(Parser.Default.ParseArguments<Options>(args));
-        //    Console.WriteLine(h.ToString());
-        //    return;
-        //}
+        Parser.Default.ParseArguments<ExtractionOptions, Options>(args)
+            .WithParsed<ExtractionOptions>(o =>
+            {
+                if (!Directory.Exists(o.InputDir))
+                {
+                    Console.WriteLine("Invalid input directory");
+                    return;
+                }
 
-        Parser.Default.ParseArguments<Options>(args)
+                ExtractMissionTL(o.InputDir, o.OutputDir, o.LanguageCode, o.Overwrite);
+
+            })
             .WithParsed<Options>(o =>
             {
                 if (!Directory.Exists(o.InputDir))
@@ -49,16 +66,106 @@ internal class Program
             });
     }
 
+    public static void ExtractMissionTL(string inputDir, string outputDir, string lang, bool overwrite)
+    {
+        Dictionary<string, string> missionCodes = new()
+        {
+            { "ME", "EFF" },
+            { "MZ", "Zeon" },
+            {"AA", "Ace_Amuro_Ray" }, //EFF
+            {"AB", "Ace_Bernard Wiseman" }, //Zeon War in The Pocekt (Zaku Kai)
+            {"AC", "Ace_Char_Aznable" }, //Zeon
+            {"AG", "Ace_Gaia" }, //Zeon (Black Tri Star)
+            {"AH", "Ace_Akahana" }, //Zeon MSG, demolition expert in Acguy
+            {"AK", "Ace_Christina_Mackenzie" }, //EFF War In The Pocket (Alex NT1)
+            {"AN", "Ace_Norris_Packard" }, //Zeon
+            {"AR", "Ace_Ramba_Ral" }, //Zeon
+            {"AS", "Ace_Shiro_Amada" }, //EFF 8th MS team
+            {"AY", "Ace_You_Kajima" }, //EFF blue destiny
+            {"CE", "Combination_EFF" }, //EFF
+            {"CZ", "Combination_Zeon" }, //Zeon
+        };
+
+        var dirs = Directory.EnumerateDirectories(inputDir, "BR_*_text.arc", SearchOption.AllDirectories); ;
+
+        foreach (var missionDir in dirs)
+        {
+            var arcName = Path.GetFileName(missionDir);
+            var missionCode = arcName.Substring(3, 2);
+            var missionNumber = arcName.Substring(5, 2);
+            List<tlentry> ChatTL = new List<tlentry>();
+            List<tlentry> ObjectivesAndIntel = new List<tlentry>();
+
+            var chatfile = Path.Combine(missionDir, $"arc/CH_{missionCode}{missionNumber}.arc/arc/BlockText.xbf.xml");
+            var objectivefile = Path.Combine(missionDir, $"arc/OP_{missionCode}{missionNumber}.arc/arc/BlockText.xbf.xml");
+            var intelfile = Path.Combine(missionDir, $"arc/BR_{missionCode}{missionNumber}.arc/arc/BlockText.xbf.xml");
+            ExtractTLEntries(ChatTL, chatfile);
+            ExtractTLEntries(ObjectivesAndIntel, objectivefile);
+            ExtractTLEntries(ObjectivesAndIntel, intelfile);
+
+            if (ChatTL.Any())
+            {
+                var json = JsonConvert.SerializeObject(ChatTL, Newtonsoft.Json.Formatting.Indented);
+                var path = Path.Combine(outputDir, "Unique/_2d/Briefing", arcName, $"dict-{missionCodes[missionCode]}_{missionNumber}-chat.{lang}.json");
+                if (!File.Exists(path) || overwrite)
+                {
+                    if (!Directory.Exists(Path.GetDirectoryName(path)))
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(path));
+                    }
+                    File.WriteAllText(path, json);
+                }
+            }
+            if (ObjectivesAndIntel.Any())
+            {
+                var json = JsonConvert.SerializeObject(ObjectivesAndIntel, Newtonsoft.Json.Formatting.Indented);
+                var path = Path.Combine(outputDir, "Unique/_2d/Briefing", arcName, $"dict-{missionCodes[missionCode]}_{missionNumber}.{lang}.json");
+                if (!File.Exists(path) || overwrite)
+                {
+                    if (!Directory.Exists(Path.GetDirectoryName(path)))
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(path));
+                    }
+                    File.WriteAllText(path, json);
+                }
+            }
+        }
+
+    }
+
+    private static void ExtractTLEntries(List<tlentry> ChatTL, string chatfile)
+    {
+        if (File.Exists(chatfile))
+        {
+            var xmldoc = new XmlDocument();
+            xmldoc.Load(chatfile);
+
+            var blocks = xmldoc.SelectNodes("/Texts/Block");
+            foreach (XmlElement block in blocks)
+            {
+                var id = block.SelectSingleNode("ID").InnerText;
+                var text = block.SelectSingleNode("Text").InnerText;
+
+                var entry = new tlentry()
+                {
+                    ID = block.SelectSingleNode("ID").InnerText,
+                    Lines = block.SelectSingleNode("Text").InnerText.Split("\n").ToArray(),
+                };
+                ChatTL.Add(entry);
+            }
+        }
+    }
+
     const string TranslationDictFilenameMask = "dict*.{0}.json";
     public static void ArcPatchEerything(string inputDir, string outputDir, string patchDir, string languageCode, bool rewrite, bool clean)
     {
-        if(Directory.Exists(outputDir) && clean)
+        if (Directory.Exists(outputDir) && clean)
         {
             Directory.Delete(outputDir, true);
         }
 
         //get global dicts first
-        Dictionary<string, windowjsonentry> globaldict = GetTLDict(Path.Combine(patchDir, UniqueDir), languageCode);
+        Dictionary<string, tlentry> globaldict = GetTLDict(Path.Combine(patchDir, UniqueDir), languageCode);
 
         var files = new System.IO.DirectoryInfo(inputDir)
             .GetFiles("*.arc", SearchOption.AllDirectories);
@@ -72,7 +179,7 @@ internal class Program
             root.Parse(bytes);
 
             var arcPath = Path.GetRelativePath(inputDir, file.FullName);
-            var updated = UpdateU8Root(root, Path.Combine(patchDir, UniqueDir, arcPath), Path.Combine(patchDir, CommonDir), Path.Combine(inputDir, arcPath), languageCode, new List<Dictionary<string, windowjsonentry>>() { globaldict });
+            var updated = UpdateU8Root(root, Path.Combine(patchDir, UniqueDir, arcPath), Path.Combine(patchDir, CommonDir), Path.Combine(inputDir, arcPath), languageCode, new List<Dictionary<string, tlentry>>() { globaldict });
 
             if (updated || rewrite)
             {
@@ -85,23 +192,23 @@ internal class Program
         }
     }
 
-    private static Dictionary<string, windowjsonentry> GetTLDict(string patchDir, string languageCode)
+    private static Dictionary<string, tlentry> GetTLDict(string patchDir, string languageCode)
     {
         if (!Directory.Exists(patchDir))
         {
-            return new Dictionary<string, windowjsonentry>();
+            return new Dictionary<string, tlentry>();
         }
 
         var paths = Directory
             .EnumerateFiles(patchDir, string.Format(TranslationDictFilenameMask, languageCode), SearchOption.TopDirectoryOnly);
 
-        Dictionary<string, windowjsonentry> result = new Dictionary<string, windowjsonentry>();
+        Dictionary<string, tlentry> result = new Dictionary<string, tlentry>();
 
         //allow for multi-file dicts
         foreach (var path in paths)
         {
             var json = File.ReadAllText(path);
-            var dict = JsonConvert.DeserializeObject<List<windowjsonentry>>(json);
+            var dict = JsonConvert.DeserializeObject<List<tlentry>>(json);
 
             foreach (var entry in dict)
             {
@@ -112,7 +219,7 @@ internal class Program
         return result;
     }
 
-    public class windowjsonentry
+    public class tlentry
     {
         public string ID { get; set; }
         public string Text { get; set; }
@@ -132,9 +239,9 @@ internal class Program
 
     const string CommonDir = "Common";
     const string UniqueDir = "Unique";
-    private static bool UpdateU8Root(U8RootSegment root, string patchDir, string commonDir, string inputDir, string languageCode, List<Dictionary<string, windowjsonentry>> dicts)
+    private static bool UpdateU8Root(U8RootSegment root, string patchDir, string commonDir, string inputDir, string languageCode, List<Dictionary<string, tlentry>> dicts)
     {
-        dicts = new List<Dictionary<string, windowjsonentry>>(dicts) //clone list
+        dicts = new List<Dictionary<string, tlentry>>(dicts) //clone list
         {
             //append new dict
             GetTLDict(patchDir, languageCode)
