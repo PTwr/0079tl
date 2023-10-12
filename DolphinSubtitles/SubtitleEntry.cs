80d2e8215a4399fa8de722db769b457460f0c15a
PTwr <PTwr@outlook.com>
2023-10-12 15:54:03 +0100

    MZ14 MZ16 MZ18 MZ20

--- DolphinSubtitles/SubtitleEntry.cs
+++ DolphinSubtitles/SubtitleEntry.cs
@@ -5,7 +5,7 @@
         public string FileName { get; set; }
         public string Translation { get; set; }
         public uint Miliseconds { get; set; }
-        public uint Color { get; set; }
+        public string Color { get; set; }
         public bool Enabled { get; set; }
         public bool AllowDuplicate { get; set; }
         public float Scale { get; set; }
--- Tests/GevTests.cs
+++ Tests/GevTests.cs
@@ -1,9 +1,13 @@
-﻿using GEVLib.GEV;
+﻿using DolphinSubtitles;
+using GEVLib.GEV;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Tests
 {
@@ -34,11 +38,16 @@ namespace Tests
                 .LoadFlattenedDicionary("../../../../Patcher/Subtitles", true, "en")
                 .ToDictionary(kvp => Path.GetFileNameWithoutExtension(kvp.Key), kvp => kvp.Value.Translation);
 
+            var brstmroot = @"C:\games\wii\0079\0079_jp\DATA\files\sound\stream";
+
+            var brstms = Directory.EnumerateFiles(brstmroot, "*.brstm")
+                .ToDictionary(i => Path.GetFileNameWithoutExtension(i), i => Path.GetRelativePath(brstmroot, i).Replace(@"\", "/"));
+
+            var unpackedEVCdir = @"C:\games\wii\0079\0079_unpacked\DATA\files\evc";
             var root = @"C:\games\wii\0079\0079_jp\DATA\files\event\missionevent\";
             //root = @$"{Environment.ExpandEnvironmentVariables("%userprofile%")}\Documents\Dolphin Emulator\Load\Riivolution\R79JAF_EN\event";
             foreach (var file in Directory.EnumerateFiles(root, "*.gev", SearchOption.AllDirectories))
             {
-
                 var pathstub = Path.GetRelativePath(root, file);
 
                 var expectedBytes = File.ReadAllBytes(file);
@@ -51,6 +60,70 @@ namespace Tests
                 var output = Path.Combine(@"C:\games\wii\0079\gevdump", pathstub + ".txt.");
                 Directory.CreateDirectory(Path.GetDirectoryName(output));
                 File.WriteAllText(output, str);
+
+                if (gev.STR == null) continue;
+                var evcs = gev.STR.Strings.Where(s => s.StartsWith("EVC"));
+
+                foreach (var evc in evcs)
+                {
+                    var evcpath = Path.Combine(unpackedEVCdir, @$"{evc}.arc/arc", "EvcScene.xbf.xml");
+                    if (!File.Exists(evcpath)) continue;
+
+                    var xml = new XmlDocument();
+                    xml.Load(evcpath);
+
+                    var voices = xml.SelectNodes(@"/EvcScn/Cut/Voice");
+
+                    List<SubtitleEntry> sceneSubs = new List<SubtitleEntry>();
+                    foreach (XmlNode voice in voices)
+                    {
+                        var soundname = voice.InnerText;
+
+                        if (soundname.Equals("End", StringComparison.InvariantCultureIgnoreCase)) continue;
+
+                        var soundfile = $"sound/stream/{soundname}.brstm";
+                        if (dict.ContainsKey(soundname))
+                            soundname = dict[soundname];
+
+                        var sub = new SubtitleEntry()
+                        {
+                            FileName = soundfile,
+                            Translation = soundname,
+                            Miliseconds = 5000,
+                            Enabled = true,
+                            Scale = 1,
+                            AllowDuplicate = false,
+                            Color = "Yellow",
+                        };
+                        sceneSubs.Add(sub);
+                    }
+
+                    var outputPath = $@"C:\games\wii\0079\scenesubs\{Path.GetFileNameWithoutExtension(file)}\{evc}.en.json";
+                    Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
+
+                    var json = JsonConvert.SerializeObject(sceneSubs, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText(outputPath, json);
+                }
+
+                var battlesubs = gev.STR.Strings
+                    .Where(brstms.ContainsKey)
+                    .Select(s => new SubtitleEntry()
+                    {
+                        FileName = "sound/stream/" + brstms[s],
+                        Translation = dict.ContainsKey(s) ? dict[s] : s,
+                        Miliseconds = 5000,
+                        Enabled = true,
+                        Scale = 1,
+                        AllowDuplicate = false,
+                        Color = "Yellow",
+                    });
+
+                {
+                    var outputPath = $@"C:\games\wii\0079\battlesubs\{Path.GetFileNameWithoutExtension(file)}.en.json";
+                    Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
+                    var json = JsonConvert.SerializeObject(battlesubs, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText(outputPath, json);
+                }
             }
             //var expectedBytes = File.ReadAllBytes(@"C:\Users\LordOfTheSkrzynka\Documents\Dolphin Emulator\Load\Riivolution\R79JAF_EN\event\missionevent\other\TR01.gev");
 
@@ -96,16 +169,16 @@ namespace Tests
             var strings = gev.STR.ToString();
 
             Assert.Equal("EVC_TU_003", gev.STR.Strings.Last());
-            Assert.Equal("EVC_TU_003", gev.STR[0x02D0*4]);
+            Assert.Equal("EVC_TU_003", gev.STR[0x02D0 * 4]);
             Assert.Equal("EVC_TU_003", gev.OFS[55]);
         }
 
-        [Fact] 
+        [Fact]
         public void ReadWriteReverse()
         {
             var tr01bytes = File.ReadAllBytes(TR01);
             var eveBytes = tr01bytes.Take(0x1bb0).Skip(0x20 - 4).ToArray();
-            var ofsBytes = tr01bytes.Take(0x1c20+4).Skip(0x1bb0).ToArray();
+            var ofsBytes = tr01bytes.Take(0x1c20 + 4).Skip(0x1bb0).ToArray();
             var strBytes = tr01bytes.Skip(0x1c20 + 4).ToArray();
 
             var gev = new GEVBinaryRootSegment();
