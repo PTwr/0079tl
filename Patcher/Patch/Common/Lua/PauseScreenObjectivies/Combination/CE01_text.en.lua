c2d1af9ef4b8bfa2c467567d5352cccc17690498
PTwr <PTwr@outlook.com>
2023-10-13 00:28:29 +0100

    Combination mission objectives

---
+++ Patcher/Patch/Common/Lua/PauseScreenObjectivies/Combination/CE01_text.en.lua
@@ -0,0 +1,12 @@
+MissionText = {
+	{
+		code = "CE01_EV01",
+		value = "New enemy weapon spotted!\
+Destroy it before it\
+anihilates our army!",
+	},
+	{
+		code = "CE01_EV02",
+		value = "Search and Destroy",
+	},
+}
---
+++ Patcher/Patch/Common/Lua/PauseScreenObjectivies/Combination/CZ01_text.en.lua
@@ -0,0 +1,22 @@
+MissionText = {
+	{
+		code = "CZ01_EV01",
+		value = "Gallop has broken down.\
+Defend it until repairs are complete!",
+	},
+	{
+		code = "CZ01_EV02",
+		value = "Enemy reinforcements arrived\
+Destroy them",
+	},
+	{
+		code = "CZ01_EV03",
+		value = "Gallop repair comleted!\
+Escort Gallow out of combat zone."
+	},
+	{
+		code = "CZ01_EV04",
+		value = "Enemy Ace unit approaching!\
+Destroy them!",
+	},
+}
